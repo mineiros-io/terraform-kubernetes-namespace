7cca7e73299afedeb5e381ebddf842e0c103aff7
Soren Martius <soeren.martius@gmail.com>
2021-11-03 10:35:30 +0100

    feat: add support for the kubernetes_namespace resource

--- variables.tf
+++ variables.tf
@@ -3,11 +3,27 @@
 # These variables must be set when using this module.
 # ----------------------------------------------------------------------------------------------------------------------
 
+variable "name" {
+  description = "(Required) Name of the namespace, must be unique. Cannot be updated. For details please see https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names"
+  type        = string
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These variables have defaults, but may be overridden.
 # ----------------------------------------------------------------------------------------------------------------------
+
+variable "labels" {
+  description = "(Optional) Map of string keys and values that can be used to organize and categorize (scope and select) namespaces. May match selectors of replication controllers and services."
+  type        = map(string)
+  default     = {}
+}
+
+variable "annotations" {
+  description = "(Optional) An unstructured key value map stored with the namespace that may be used to store arbitrary metadata."
+  type        = map(string)
+  default     = {}
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULE CONFIGURATION PARAMETERS
